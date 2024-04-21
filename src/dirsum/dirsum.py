3b50ba8e5e2cd4e5e6457ee1981dcbad3572552f
GeorgeBannister <gbannister206@gmail.com>
2024-04-21 20:03:14 +0100

    Better contrast on pretty mode

--- src/dirsum/dirsum.py
+++ src/dirsum/dirsum.py
@@ -20,9 +20,9 @@ def colour_hash_str(digest: str) -> str:
     :param digest: A hex digest str
     :returns: `digest` with ANSI escape codes to add colour
     """
-    seed = int(digest, base=16) % 256
-    opposite_seed = (seed - 128) % 128
-    return f"\033[38:5:{seed}m\033[48:5:{opposite_seed}:m{digest}\033[m"
+    background = int(digest, base=16) % 256
+    foreground = 15 if background in (0, 1, 52, 88, 232, 233, 234, 235, 236, 237) else 0
+    return f"\033[38:5:{foreground}m\033[48:5:{background}:m{digest}\033[m"
 
 
 def parse_args() -> Config:
